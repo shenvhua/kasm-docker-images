caa35e64bb8e2cfeffc8a11ce27817fdffbd2f25
clarkchen <clark@clarkke.com>
2024-03-02 17:44:42 +0800

    change

---
+++ src/ubuntu/install/obsidian/install_obsidian.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -xe
+
+# Install obsidian
+OBSIDIAN_VERSION=1.5.8
+wget -q -O obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb
+apt-get update
+apt-get install -y ./obsidian.deb
+rm obsidian.deb
+
+
+
+
+
+# Cleanup
+if [ -z ${SKIP_CLEAN+x} ]; then
+    apt-get autoclean
+    rm -rf \
+        /var/lib/apt/lists/* \
+        /var/tmp/* \
+        /tmp/*
+fi
+
+# Cleanup for app layer
+chown -R 1000:0 $HOME
+find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
