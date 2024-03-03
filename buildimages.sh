892af7862f4219338e71f8036d5f2c8c157b3936
clarkchen <clark@clarkke.com>
2024-03-03 15:00:26 +0800

    change

--- buildimages.sh
+++ buildimages.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 docker build --platform linux/amd64 -t obsidian:dev -f dockerfile-kasm-obsidian .
+
+
+
+docker build --platform linux/amd64 -t obsidian:dev -f dockerfile-kasm-obsidian .
 docker run --rm  -it --shm-size=512m -p 6901:6901 -e VNC_PW=123456 obsidian:dev
--- obsidian/src/ubuntu/install/obsidian/install_obsidian.sh
+++ obsidian/src/ubuntu/install/obsidian/install_obsidian.sh
@@ -3,6 +3,7 @@ set -xe
 
 # Install obsidian
 OBSIDIAN_VERSION=1.5.8
+#wget -q -O obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb
 wget -q -O obsidian.deb https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb
 apt-get update
 apt-get install -y ./obsidian.deb
@@ -12,15 +13,15 @@ rm obsidian.deb
 
 
 
-# Cleanup
-if [ -z ${SKIP_CLEAN+x} ]; then
-    apt-get autoclean
-    rm -rf \
-        /var/lib/apt/lists/* \
-        /var/tmp/* \
-        /tmp/*
-fi
+
 
 # Cleanup for app layer
 chown -R 1000:0 $HOME
 find /usr/share/ -name "icon-theme.cache" -exec rm -f {} \;
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/* \
+    /tmp/*
+fi
